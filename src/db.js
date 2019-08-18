abc68b85fd4efe92fdb985dbcef316d855571a12
Igor Ovinov <market1942@mail.ru>
2019-08-18 17:13:34 +0300

    commit

---
+++ src/db.js
@@ -0,0 +1,84 @@
+ let products=[
+        {
+            "title": "becon",
+            "category": "meat",
+            "price":10,
+             "id":1
+            
+        },
+        {
+            "title": "chicken",
+            "category": "meat",
+            "price":13,
+             "id":2
+            
+        },
+        {
+            "title": "steak",
+            "category": "meat",
+            "price":23 ,
+             "id":3
+            
+        },
+        {
+            "title": "carrot",
+            "category": "vegetable",
+            "price":7,
+             "id":4
+            
+        },
+        {
+            "title": "onion",
+            "category": "vegetable",
+            "price":3,
+             "id":5
+            
+        },
+        {
+            "title": "tomat",
+            "category": "vegetable",
+            "price":6,
+             "id":6
+            
+        },
+        {
+            "title": "broccoli",
+            "category": "vegetable",
+            "price":7,
+             "id":7
+  
+            
+        },
+        {
+            "title": "corn",
+            "category": "vegetable",
+            "price":9,
+             "id":8
+            
+        },
+        {
+            "title": "banana",
+            "category": "fruit",
+            "price":7,
+             "id":9
+        },
+        {
+            "title": "mango",
+            "category": "fruit",
+            "price":16,
+             "id":10
+        },
+        {
+            "title": "apple",
+            "category": "fruit",
+            "price":9,
+             "id":11
+        },
+        {
+            "title": "melon",
+            "category": "fruit",
+            "price":12,
+             "id":12
+        }
+    ]
+    export default products
